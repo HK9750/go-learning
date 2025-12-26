7fe3b1f3040264b05a09a9e5187160c08afd1d2b
HK9750 <hasnainkhan9750@gmail.com>
2025-12-26 22:42:29 +0500

    feat: Collect concurrent URL check results via a channel and defer response body closure.

--- exercises/04_url_checker/main.go
+++ exercises/04_url_checker/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"sync"
 )
@@ -10,33 +11,37 @@ type result struct {
 	status string
 }
 
-func check(url string, wg *sync.WaitGroup) result {
+func check(url string, wg *sync.WaitGroup, results chan result) {
 	defer wg.Done()
 	resp, err := http.Get(url)
 	if err != nil {
-		return result{url: url, status: "ERROR"}
+		results <- result{url: url, status: "ERROR"}
+		return
 	}
-	return result{url: url, status: resp.Status}
+	defer resp.Body.Close()
+	results <- result{url: url, status: resp.Status}
 }
 
 func main() {
-	urls := []string{"http://site1.com",
-		"http://site2.com",
-		"http://site3.com",
-		"http://site4.com",
-		"http://site5.com",
-		"http://site6.com",
-		"http://site6.com",
-		"http://site7.com",
-		"http://site8.com",
-		"http://site9.com",
-		"http://site10.com"}
+	urls := []string{"http://google.com", "http://facebook.com", "http://twitter.com"}
 
 	var wg sync.WaitGroup
 
+	results := make(chan result)
+
 	for _, url := range urls {
 		wg.Add(1)
-		go check(url, &wg)
+		go check(url, &wg, results)
 	}
-	wg.Wait()
+
+	go func() {
+		wg.Wait()
+		close(results)
+	}()
+
+	fmt.Printf("Results chan type : %T, results chan value %v \n", results, results)
+
+	for result := range results {
+		fmt.Println(result)
+	}
 }
